cc2b8fb636f242ea8fb086015585fdab5129f09c
Pikohunter22 <57916276+Pikohunter22@users.noreply.github.com>
2019-11-21 21:34:36 +0100

    Add files via upload

---
+++ Crnogorski uno.cpp
@@ -0,0 +1,170 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+	
+	void crvenoDevet();
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  9  * CRVENO *  9  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *  *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  9  * CRVENO *  9  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	
+	
+	void crvenoOsam();
+	
+	cout<<"**********************"<<endl;
+	 cout<<"*  8  * CRVENO *  8  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *  *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *  *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  8  * CRVENO *  8  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 
+	 void crvenoSedam();
+	 
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  7  * CRVENO *  7  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  7  * CRVENO *  7  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 
+	 void crvenoSest();
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  6  * CRVENO *  6  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *           *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *  *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  6  * CRVENO *  6  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 void crvenoPet();
+	 
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  5  * CRVENO *  5  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *           *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  5  * CRVENO *  5  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 
+	 void crvenoCetri();
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  4  * CRVENO *  4  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        *           *"<<endl;
+	 cout<<"*        *           *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  4  * CRVENO *  4  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 void crvenoTri();
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  3  * CRVENO *  3  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  3  * CRVENO *  3  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 void crvenoDva();
+	 
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  2  * CRVENO *  2  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*        *           *"<<endl;
+	 cout<<"*        ****        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  2  * CRVENO *  2  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 
+	 void crvenoJedan();
+	 
+	 
+	 cout<<"**********************"<<endl;
+	 cout<<"*  1  * CRVENO *  1  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*           *        *"<<endl;
+	 cout<<"*                    *"<<endl;
+	 cout<<"**********************"<<endl;
+	 cout<<"*  1  * CRVENO *  1  *"<<endl;
+	 cout<<"**********************"<<endl;
+	 
+	 system("CLS");
+	 cout<<"Dobrodosao u crnogorski uno!"<<endl;
+	 
+}
